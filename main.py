df6e24535aa4789b46e35be57153f7fae4d7e5fd
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-07-18 19:53:02 +0000

    eu tentei

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 def q1():
+    ano = int(input(''))
+    periodico = int(input(''))
+    anos = (f'{ano + periodico} {ano + periodico * 2} {ano + periodico * 3} {ano + periodico * 4} ')
+    print(anos)
     pass
 
 def q2():
     pass
+                
+            
+
+        
 
 def q3():
-    pass
+    vaquinha = 0
+    clientes = 0
+    dinheiro = 0
+    while dinheiro >= -1:
+        dinheiro == float(input(''))
+        clientes += 1
+        vaquinha += dinheiro
+    media = vaquinha / clientes
+    print(media, vaquinha)
+
 
 def q4():
-    pass
-
+    dias = float(input(''))
+    taxa = 14
+    if dias == 0:
+        print("Valor inválido")
+        pass
+    km = float(input(''))
+    pagar = 90
+    if km > 100:
+        mais += km - 100 
+        pagar += mais * taxa
+    elif dias > 1 :
+        print(pagar)
+        pass
+    print(f"{pagar:>10.2f}")
 def q5():
     pass
 
 if __name__=="__main__":
     # teste sua questão manualmente aqui
+    pass
